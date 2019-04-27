e5a952fbeb65001383a87b4a8abafb00c8867cf0
antikov <crazytraffic@gmail.com>
2019-04-27 21:33:08 +0500

    solved 14

---
+++ day14/solution_1.py
@@ -0,0 +1,18 @@
+INPUT = 440231
+
+def main():
+    recipes = [3, 7]
+    first = 0
+    second = 1
+    while len(recipes) < INPUT + 10:
+        total = recipes[first] + recipes[second]
+        if total > 9:
+            recipes.extend([1, total % 10])
+        else:
+            recipes.append(total)
+        first = (first + recipes[first] + 1) % len(recipes)
+        second = (second + recipes[second] + 1) % len(recipes)
+    print("".join([str(digit) for digit in recipes[INPUT:INPUT+10]]))
+
+if __name__ == "__main__":
+    main()
---
+++ day14/solution_2.py
@@ -0,0 +1,23 @@
+# it took ~1 min to get answer
+
+INPUT = "440231"
+
+def get_last(recipes, offset = 0):
+    return "".join([str(digit) for digit in recipes[-len(INPUT) - offset:-offset]])
+
+def main():
+    recipes = [3, 7]
+    first = 0
+    second = 1
+    while get_last(recipes) != INPUT and get_last(recipes, offset=1) != INPUT:
+        total = recipes[first] + recipes[second]
+        if total > 9:
+            recipes.extend([1, total % 10])
+        else:
+            recipes.append(total)
+        first = (first + recipes[first] + 1) % len(recipes)
+        second = (second + recipes[second] + 1) % len(recipes)
+    print("".join([str(digit) for digit in recipes]).index(INPUT))
+
+if __name__ == "__main__":
+    main()
